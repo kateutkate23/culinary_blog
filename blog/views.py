f438fe6a9b1f81468e760901fbf1c40f96ec4279
kate_utkate <kate_utkate@vk.com>
2025-01-25 14:06:35 +0300

    started template rendering

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Category, Post
 
-# Create your views here.
+
+def index(request):
+    """Главная страница сайта"""
+    posts = Post.objects.all()
+    context = {
+        'title': 'Главная страница',
+        'posts': posts
+    }
+
+    return render(request, 'blog/index.html', context)
